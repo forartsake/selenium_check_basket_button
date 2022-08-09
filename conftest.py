8af76bafb214a977339a464a6e662ad271fe8459
forartsake <a.yunchits@gmail.com>
2022-08-09 22:21:35 +0200

    chrome added as a default browser

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ def pytest_addoption(parser):
     parser.addoption(
         '--browser_name',
         action='store',
-        default=None,
+        default='chrome',
         help="Choose browser: chrome or firefox"
                      )
     parser.addoption(
